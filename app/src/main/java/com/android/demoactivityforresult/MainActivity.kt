90b22c0d72f604cd4b8722eed152a2b55e1879c8
Nguyen Van Linh <linhvan@bravesoft.com.vn>
2021-01-13 15:14:59 +0700

    - Bonus demo code.

--- app/src/main/java/com/android/demoactivityforresult/MainActivity.kt
+++ app/src/main/java/com/android/demoactivityforresult/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.demoactivityforresult.fragment.FragmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +30,32 @@ class MainActivity : AppCompatActivity() {
         btnOpenFragment.setOnClickListener {
             startActivity(Intent(this, FragmentActivity::class.java))
         }
+        /*
+        Bonus
+        val takePicture = registerForActivityResult(
+            ActivityResultContracts.TakePicturePreview(),
+            activityResultRegistry
+        ) { bitmap: Bitmap? ->
+            Log.d("takePicture", "bitmap")
+        }
+        takePicture.launch(null)
 
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            }
+         requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        */
     }
 }
